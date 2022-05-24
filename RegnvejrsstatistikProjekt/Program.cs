3d7dcafe61f1f900506a55ebc502a4ff013f7c18
Elias Nielsen Andersen <elia1040@elevcampus.dk>
2022-05-24 14:19:14 +0200

    input

--- RegnvejrsstatistikProjekt/Program.cs
+++ RegnvejrsstatistikProjekt/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Antal måleværdier: ");
+int _antalMåleværdier;
+while (!int.TryParse(Console.ReadLine(), out _antalMåleværdier))
+{
+    Console.Clear();
+    Console.Write("Indtast et tal: ");
+}
+
+double[] _måleværdier = new double[_antalMåleværdier];
+for (int i = 0; i < _måleværdier.Length; i++)
+{
+    Console.Write($"Måleværdi for dag [{i + 1}]: ");
+    while (!double.TryParse(Console.ReadLine(), out _måleværdier[i]))
+    {
+        Console.Clear();
+        Console.Write("Indtast et tal: ");
+    }
+}
